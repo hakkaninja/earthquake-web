97e3d7a68231244b0fb506e18911a5414c316e68
hakkaninja <33673576+hakkaninja@users.noreply.github.com>
2018-06-12 22:12:30 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function nextPage(e) {
+    if (e.keyCode === 13) {
+        
+        location.href = 'summary.html';
+        // run api call function 
+        // if going back then clear input box (ie refresh page)
+    }
+}
